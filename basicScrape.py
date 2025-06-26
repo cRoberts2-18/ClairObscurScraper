398d8fb88379b84bde69540c491b81a9f6ca518d
cRoberts2-18 <callumr5@hotmail.co.uk>
2025-06-26 19:51:11 +0100

    Picto scraper v2

--- basicScrape.py
+++ basicScrape.py
@@ -4,14 +4,14 @@ import pandas as pd
 import itertools
 
 
-def getGetDataForPicto(text):
-    text = text.replace(' ', '+')
+def getGetDataForPicto(name):
+    text = name.replace(' ', '+')
     url = "https://expedition33.wiki.fextralife.com/" + text
     resp = requests.get(url)
     tree = etree.HTML(resp.text)
     val = tree.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div/div/div[2]/div/div[4]/div[1]/ul[1]//text()')
     val = ''.join(val)
-    return val
+    return [name, url, val]
 
 res = pd.read_csv('baseData.csv', sep=',', header=None)
 
@@ -19,7 +19,7 @@ data = list(itertools.chain.from_iterable(res.values))
 data = list(map(getGetDataForPicto, data))
 print(data)
 
-df = pd.DataFrame(data, columns=["colummn"])
+df = pd.DataFrame(data, columns=["name", "url", "location"])
 df.to_csv('picto_locations.csv', index=False)
 
 
